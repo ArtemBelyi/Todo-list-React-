cc8e2643069044fa8b29702e78b121de6661e4c5
Artem <abelyi467@gmail.com>
2021-10-08 14:21:24 +0300

    create module(AppHeader, SearchPanel, TodoList, TodoListItem)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("New Project")
+import React from 'react';
+import ReactDom from 'react-dom';
+import AppHeader from './components/AppHeader';
+import TodoList from './components/TodoList';
+import SearchPanel from './components/SearchPanel';
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+        </div>
+    )
+}
+
+ReactDom.render(<App/>, document.getElementById('root'))
