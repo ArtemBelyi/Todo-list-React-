f0561424b846fc752a0e4d97ae91046f13f4ba33
Artem <abelyi467@gmail.com>
2021-10-11 15:21:06 +0300

    add onLabelClick in TodolistItem

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -2,15 +2,24 @@ import React from 'react';
 import './TodoListItem.css'
 
 export default class TodolistItem extends React.Component {
+
+    state = { done: false, important: false}
+
+    onLabelClick = () => {
+        this.setState({ done: true })
+    }
+    onImportantClick = () => {
+        this.setState({ important: true })
+    }
     render() {
         const { label, important = false } = this.props
-        const style = {
-            color: important ? "steelblue" : "black"
-        }
+        let className = 'todo-list-item d-flex'
+        if(this.state.done) className += ' done'
+        if(this.state.important) className += ' important'
         return (
-            <span className="todo-list-item d-flex">
-                <span className="todo-list-item-label" style = { style }>{ label }</span>
-                <button type="button" className="btn btn-outline-success btn-sm todo-list-item__button"><i className="bi bi-exclamation-octagon"></i></button>
+            <span className={ className }>
+                <span className="todo-list-item-label" onClick={ this.onLabelClick }>{ label }</span>
+                <button type="button" className="btn btn-outline-success btn-sm todo-list-item__button" onClick={ this.onImportantClick }><i className="bi bi-exclamation-octagon"></i></button>
                 <button type="button" className="btn btn-outline-danger btn-sm todo-list-item__button"><i className="bi bi-cart-x"></i></button>
             </span>
         )
